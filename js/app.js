86a803446d2e94bfd7d1a815a10cda8b3128c9fd
angel-20-12 <angel.lardi@gmail.com>
2020-01-25 23:34:50 +0200

    3 section

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$ (document).ready(function(){
+    $('.ba-members').slick(
+       {
+           speed: 1000,
+           autoplaySpeed: 1000,
+           autoplay: true,
+           arrows: true,  
+        //    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left'></i></button>",
+        //    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right'></i></button>",
+           slidesToShow: 3,
+           slidesToScroll: 1,
+           infinity: true,
+           centerMode: true
+       })
+
+})
